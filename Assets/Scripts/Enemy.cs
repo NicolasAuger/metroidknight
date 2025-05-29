e96e58865c13585239aad57efbfb7a6a66a55ea5
Nicolas AUGER <nicolas@poool.fr>
2025-05-29 12:51:25 +0200

    fix: fix indent

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -23,28 +23,28 @@ public class Enemy : MonoBehaviour
     protected Animator animator;
 
 
-        protected enum EnemyStates
-        {
-            // Crawler
-            Crawler_Idle,
-            Crawler_Flip,
+    protected enum EnemyStates
+    {
+        // Crawler
+        Crawler_Idle,
+        Crawler_Flip,
 
-            // Bat
-            Bat_Idle,
-            Bat_Chase,
-            Bat_Stunned,
-            Bat_Death,
+        // Bat
+        Bat_Idle,
+        Bat_Chase,
+        Bat_Stunned,
+        Bat_Death,
 
-            // Charger
-            Charger_Idle,
-            Charger_Surprised,
-            Charger_Charge,
-        
-            // Shade
-            Shade_Idle,
-            Shade_Chase,
-            Shade_Stunned,
-            Shade_Death
+        // Charger
+        Charger_Idle,
+        Charger_Surprised,
+        Charger_Charge,
+    
+        // Shade
+        Shade_Idle,
+        Shade_Chase,
+        Shade_Stunned,
+        Shade_Death
 
      };
     protected EnemyStates currentEnemyState;
