6e574ccc15d17435496bef9ac3055f25d6b26a65
Nicolas AUGER <nicolas@poool.fr>
2025-05-29 12:50:31 +0200

    feat: save shade position while transitionning to another scene

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -4,32 +4,47 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Metroknight {
-public class SceneTransition : MonoBehaviour
-{
-    [SerializeField] private string transitionTo;
-    [SerializeField] private Transform startPoint;
-    [SerializeField] private Vector2 exitDirection;
-    [SerializeField] private float exitTime;
-
-    private void Start()
+    public class SceneTransition : MonoBehaviour
     {
-        if (transitionTo == GameManager.Instance.transitionedFromScene) {
-            PlayerController.Instance.transform.position = startPoint.position;
-            // Fix weird inverted direction while changing scenes loading
-            StartCoroutine(PlayerController.Instance.WalkIntoNewScene(exitDirection, exitTime));
+        [SerializeField] private string transitionTo;
+        [SerializeField] private Transform startPoint;
+        [SerializeField] private Vector2 exitDirection;
+        [SerializeField] private float exitTime;
+
+        private void Start()
+        {
+            if (transitionTo == GameManager.Instance.transitionedFromScene)
+            {
+                PlayerController.Instance.transform.position = startPoint.position;
+                // Fix weird inverted direction while changing scenes loading
+                StartCoroutine(PlayerController.Instance.WalkIntoNewScene(exitDirection, exitTime));
+            }
+            StartCoroutine(UIManager.Instance.sceneFader.Fade(SceneFader.FadeDirection.Out));
         }
-        StartCoroutine(UIManager.Instance.sceneFader.Fade(SceneFader.FadeDirection.Out));
-    }
 
 
-    private void OnTriggerEnter2D(Collider2D _other)
-    {
-        if (_other.CompareTag("Player")) {
-            GameManager.Instance.transitionedFromScene = SceneManager.GetActiveScene().name;
-            PlayerController.Instance.pState.cutscene = true;
-            PlayerController.Instance.pState.invincible = true;
-            StartCoroutine(UIManager.Instance.sceneFader.FadeAndLoadScene(SceneFader.FadeDirection.In, transitionTo));
+        private void OnTriggerEnter2D(Collider2D _other)
+        {
+            if (_other.CompareTag("Player"))
+            {
+                CheckShadeData();
+                GameManager.Instance.transitionedFromScene = SceneManager.GetActiveScene().name;
+                PlayerController.Instance.pState.cutscene = true;
+                PlayerController.Instance.pState.invincible = true;
+                StartCoroutine(UIManager.Instance.sceneFader.FadeAndLoadScene(SceneFader.FadeDirection.In, transitionTo));
+            }
         }
-    }
+
+        void CheckShadeData()
+        {
+            GameObject[] enemyObjects = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyObjects)
+            {
+                if (enemy.GetComponent<Shade>() != null)
+                {
+                    SaveData.Instance.SaveShade();
+                }
+            }
+        }
 }
 }
